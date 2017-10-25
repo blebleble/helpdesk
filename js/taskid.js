1ddad5ea0e72d51e48b5e42204eae1597e7bf7f6
srututututu <ola.stolorz@vp.pl>
2017-10-25 14:56:29 +0200

    hjhjhjh

--- js/taskid.js
+++ js/taskid.js
@@ -6,12 +6,17 @@ function getID(){
 	$( "#select_task" ).change(function() {
 	var d = document.getElementById("select_task").value;	
 	$.post( "get_subtasks.php", {data: d} )
-	.done(function( dd ) {
-    alert( "Data Loaded: " + dd );
+	.done(function(jsonArray ) {
+    
 	
+	var names = JSON.parse(jsonArray);
+			 var j=1;
+			 while (names[j]) {
+				$( "#subtasks_list").prepend("<option value='"+names[j]+"'>");
+			 j++;
+			 }
 	
-	
-		
-		
+			
     });    
 })}
+
