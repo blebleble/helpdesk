a40aa09ad80ae6d6853ada82fc2ab47de92e6ff8
qpalzmxnskwo <32862087+qpalzmxnskwo@users.noreply.github.com>
2017-10-25 13:59:03 +0200

    .

--- js/taskid.js
+++ js/taskid.js
@@ -5,18 +5,12 @@ jQuery(document).ready(function(){
 function getID(){
 	$( "#select_task" ).change(function() {
 	var d = document.getElementById("select_task").value;	
-	 $.ajax( // wywołanie ajaxa
-      { 
-         type: "POST",
-         url: "get_subtasks.php",
-          data:d,
-         cache: false,
-         success: function(){}        
-      })
+	$.post( "get_subtasks.php", {data: d} )
+	.done(function( dd ) {
+    alert( "Data Loaded: " + dd );
 	
 	
 	
-		
 		
     });    
-}
+})}
