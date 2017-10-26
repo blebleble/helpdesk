4384305dc04cfd0b39784e920ec26ed66b8b8c31
qpalzmxnskwo <32862087+qpalzmxnskwo@users.noreply.github.com>
2017-10-26 10:44:04 +0200

    Revert "."
    This reverts commit f38007d92dc7e19649aa89c3ceb82ee144e6dc27.

--- get_subtasks.php
+++ get_subtasks.php
@@ -1,21 +1,6 @@
 <?php
 
-$id = $_POST["data"];
+echo $_POST["data"];
 
-require_once "database/dbinfo.php";
-require_once "database/connect.php";
-$connection = db_connection(); 
-
-$i=0;
-
-$sql="SELECT $db_subtask_name FROM $db_subtask_tab WHERE $db_subtask_taskid = $id";
-$result = $connection->query($sql);
-while($row = $result->fetch_assoc()) {
-$array[$i]=$row[$db_subtask_name];
-
-$i++;
-}
-
-echo json_encode($array);
 
 ?>
