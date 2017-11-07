99655deaf657c0cf9aeaea2afb2e43c743b82fd3
qpalzmxnskwo <32862087+qpalzmxnskwo@users.noreply.github.com>
2017-11-07 15:01:43 +0100

    .

--- team_tasks.php
+++ team_tasks.php
@@ -20,6 +20,8 @@
         
 ?>
 
+<?php include("sidebar.php"); ?>
+
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -85,35 +87,8 @@
                        ?>
           </li>
 
-  <li><a href="main.php" class="w3-bar-item w3-button">Strona główna</a></li>
-  <li><a class="w3-bar-item w3-button active-menu" name='a' style="cursor:pointer">Zadania</a></li>
-  <div class="panela">
-  <div class="hover"><li><a href="tasks.php" class="w3-bar-item w3-button sidelist">Moje aktywne zadania</a></li></div>
-  <div class="hover"><li><a href="old_tasks.php" class="w3-bar-item w3-button sidelist">Zamknięte zadania</a></li></div>
-  <div class="hover"><li><a href="suspended.php" class="w3-bar-item w3-button sidelist">Zawieszone</a></li></div>
-<?php 
-                   
-                   If ($_SESSION['function']=="2" ){
-                      echo '<div class="hover"><li><a href="team_tasks.php" class="w3-bar-item w3-button sidelist">Zadania grupy</a></li></div>';
-                      echo '<div class="hover"><li><a href="managers.php" class="w3-bar-item w3-button sidelist">Zadania innych menadżerów</a></li></div>';
-                   }
-                   ?>
-</div>
+        <?php sidebar(); ?>
 
-<?php
-                If ($_SESSION['function']=="2" ){
-                echo' <li><a class="w3-bar-item w3-button" name="b" style="cursor: pointer">Dodaj</a></li>';
-       
-  }
-  ?>
-<div class="panelb">
-    <div class="hover"><li><a href="add_tasks.php" class="w3-bar-item w3-button sidelist">Dodaj zadanie</a><li></div>
-    <div class="hover"><li><a href="add_subtasks.php" class="w3-bar-item w3-button sidelist">Dodaj podzadanie</a><li></div>
-</div>
-
-  <li><a href="search.php" class="w3-bar-item w3-button">Wyszukaj</a></li>
-               
-            </div>
             
         </nav>  
         <!-- /. NAV SIDE  -->
